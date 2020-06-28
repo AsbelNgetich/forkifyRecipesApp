4f4bda4f733c041758039c80a1a422e9101cb63b
Asbel <asbeln01@yahoo.com>
2020-06-27 19:11:37 -0500

    Completed search functionality with pagination on the results

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -14,19 +14,21 @@ const state = {};
 const controlSearch = async () => {
     //1) Get query from view
     const query = searchView.getInput();
-    //const   query = 'pasta';
-    console.log(query);
-
+    
     if (query) {
         state.search = new Search(query);
 
         //prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         //search for recipes
         await state.search.getResults();
 
+        clearLoader();
         //render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
 
     }
 }
@@ -34,6 +36,18 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+
 });
 
 
